8942662298bbed11527f25bfd8698e90e3d6a6df
WafiMo1 <78387978+WafiMo1@users.noreply.github.com>
2022-10-19 14:45:00 -0400

    initialCommit

---
+++ app/src/main/java/ca/qc/bdeb/c5gm/tp1_appmobiles/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.qc.bdeb.c5gm.tp1_appmobiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
